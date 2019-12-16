23df26a95a65a17ca4c111e3d0db316167f6d7de
Dizzy <386322135@qq.com>
2019-12-16 13:45:40 +0800

    第一次提交测试

---
+++ mall_test/src/test/java/com/rongcy/mall/mall_test/MallTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rongcy.mall.mall_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
